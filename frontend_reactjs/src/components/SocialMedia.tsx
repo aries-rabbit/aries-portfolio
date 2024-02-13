943c72746b0011a8ef8b7fc577dea4572663263c
ariesrabbit <anh.thanh1400@gmail.com>
2024-02-13 18:45:47 +0700

    update op:

--- frontend_reactjs/src/components/SocialMedia.tsx
+++ frontend_reactjs/src/components/SocialMedia.tsx
@@ -1,7 +1,13 @@
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaGitlab } from "react-icons/fa";
 const SocialMedia = () => {
   return (
     <div className="app__social">
+      <div onClick={() => window.open("https://github.com/aries-rabbit")}>
+        <FaGithub />
+      </div>
+      <div onClick={() => window.open("https://gitlab.com/ariesrabbit")}>
+        <FaGitlab />
+      </div>
       <div
         onClick={() =>
           window.open("https://www.linkedin.com/in/congthanhdao2703/")
